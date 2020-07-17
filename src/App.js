c06fca93999beaf21aad31773c4eb851023ae5e2
Sean <psytst@nottingham.ac.uk>
2020-07-18 01:58:16 +0700

    modify filtering bars

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ class App extends Component {
           <CSSTransition key={currentKey} timeout={timeout} classNames="pageSlider" mountOnEnter={false} unmountOnExit={true}>
             <div className={this.getPathDept(location) - this.state.prevDepth >= 0 ? "left" : "right"}> */}
         <Switch location={location}>
-          <Route exact path="/index" component={Home} />
+          <Route exact path="/home" component={Home} />
           <Route exact path="/404" component={FourOhFourPage} />
           <Route exact path="/my-map" component={MyMap} />
           <Route exact path="/restaurant/:res" component={RestaurantDetail} />
