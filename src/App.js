67cc338b1f90d0181ee8b43673fe7ebd3c3f3b1b
Sean <psytst@nottingham.ac.uk>
2020-07-18 00:52:39 +0700

    modify filtering bars

--- src/App.js
+++ src/App.js
@@ -34,21 +34,20 @@ class App extends Component {
     const currentKey = location.pathname.split("/")[1] || "/";
     return (
       <div>
-        <TransitionGroup component="div">
+        {/* <TransitionGroup component="div">
           <CSSTransition key={currentKey} timeout={timeout} classNames="pageSlider" mountOnEnter={false} unmountOnExit={true}>
-            <div className={this.getPathDept(location) - this.state.prevDepth >= 0 ? "left" : "right"}>
-              <Switch location={location}>
-                <Route exact path="/" component={Root} />
-                <Route exact path="/index" component={Home} />
-              </Switch>
-            </div>
-          </CSSTransition>
-        </TransitionGroup>
-        <Switch>
+            <div className={this.getPathDept(location) - this.state.prevDepth >= 0 ? "left" : "right"}> */}
+        <Switch location={location}>
+          <Route exact path="/" component={Root} />
+          <Route exact path="/index" component={Home} />
           <Route exact path="/404" component={FourOhFourPage} />
           <Route exact path="/my-map" component={MyMap} />
           <Route exact path="/restaurant/:res" component={RestaurantDetail} />
         </Switch>
+        {/* </div>
+          </CSSTransition>
+        </TransitionGroup> */}
+        <Switch></Switch>
       </div>
     );
   }
