23d55f4b6917995fee3b8e07b1a9cf9823e8bc45
Sean <psytst@nottingham.ac.uk>
2020-07-18 18:32:07 +0700

    modify filtering bars

--- src/Home.js
+++ src/Home.js
@@ -77,7 +77,7 @@ export default function Home() {
       const review = await insertReview();
     }
     // getUser();
-    getMenu();
+    // getMenu();
     // getReviews();
   }, [searchParams]);
 
